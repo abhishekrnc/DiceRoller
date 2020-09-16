5c63c49025f6af2e509c29e0a0d063e926c983da
Abhishek <abhishektupudana@gmail.com>
2020-09-16 17:51:09 +0530

    final

--- app/src/main/java/com/droidabhishek/diceroller/MainActivity.kt
+++ app/src/main/java/com/droidabhishek/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -21,7 +22,17 @@ import kotlin.random.Random
 
 
          val randomInt = java.util.Random().nextInt(6) + 1
+         val drawableResource = when (randomInt) {
+             1 -> R.drawable.dice_1
+             2 -> R.drawable.dice_2
+             3 -> R.drawable.dice_3
+             4 -> R.drawable.dice_4
+             5 -> R.drawable.dice_5
+             else -> R.drawable.dice_6
+         }
 
+         val diceImage: ImageView = findViewById(R.id.dice_image)
+         diceImage.setImageResource(drawableResource)
 
      }
  }
